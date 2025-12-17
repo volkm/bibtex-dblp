ac8a15334440b920440d71fdad13a98a3788ae5a
Matthias Volk <m.volk@tue.nl>
2025-12-17 17:24:48 +0100

    Reduce bibtex file for tests

--- tests/convert/test_convert.py
+++ tests/convert/test_convert.py
@@ -8,7 +8,7 @@ from bibtex_dblp.dblp_api import BibFormat
 def test_convert(dblp_session):
     bib = bibtex_dblp.database.load_from_file(bib_path("ley.bib"))
     bib, no_changes = bibtex_dblp.database.convert_dblp_entries(dblp_session, bib, bib_format=BibFormat.condensed_doi)
-    assert no_changes == 32
+    assert no_changes == 9
 
 
 def test_convert_invalid_id(dblp_session):
